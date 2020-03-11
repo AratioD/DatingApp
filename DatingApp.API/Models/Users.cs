c61562900ab01d64df1c17b35584f8eb8812b433
AratioD <fffb@ddd.com>
2020-03-11 19:35:08 +0200

    adding password hashes and salt

---
+++ DatingApp.API/Models/Users.cs
@@ -0,0 +1,13 @@
+namespace DatingApp.API.Models
+{
+    public class Users
+    {
+        public int Id { get; set; }
+        public string Username { get; set; }
+
+        public byte[] PasswordHash { get; set; }
+
+        public byte[] PasswordSalt { get; set; }
+
+    }
+}

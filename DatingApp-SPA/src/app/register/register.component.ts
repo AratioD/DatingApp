6c1bc7ec35381e4bdc555f6b7590cbeef3ff39f0
AratioD <fffb@ddd.com>
2020-03-29 19:48:31 +0300

    toggle button for register working and tested

--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,15 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-register',
-  templateUrl: './register.component.html',
-  styleUrls: ['./register.component.css']
+  selector: "app-register",
+  templateUrl: "./register.component.html",
+  styleUrls: ["./register.component.css"]
 })
 export class RegisterComponent implements OnInit {
+  model: any = {};
+  constructor() {}
 
-  constructor() { }
+  ngOnInit() {}
 
-  ngOnInit() {
+  register() {
+    console.log(this.model);
   }
 
+  cancel() {
+    console.log('cancelled!');
+  }
 }

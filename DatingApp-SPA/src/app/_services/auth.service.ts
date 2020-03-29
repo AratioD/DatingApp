632c8ea854b0ed050991c8752c17865c2d7bd86a
AratioD <fffb@ddd.com>
2020-03-29 20:34:06 +0300

    new user registration works

--- DatingApp-SPA/src/app/_services/auth.service.ts
+++ DatingApp-SPA/src/app/_services/auth.service.ts
@@ -19,4 +19,8 @@ export class AuthService {
       })
     );
   }
+
+  register(model: any) {
+    return this.http.post(this.baseUrl + 'register', model);
+  }
 }
--- DatingApp-SPA/src/app/register/register.component.ts
+++ DatingApp-SPA/src/app/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit, Input, Output, EventEmitter } from "@angular/core";
+import { AuthService } from '../_services/auth.service';
 
 @Component({
   selector: "app-register",
@@ -9,12 +10,16 @@ export class RegisterComponent implements OnInit {
   @Input() valuesFromHome: any;
   @Output() cancelRegister = new EventEmitter();
   model: any = {};
-  constructor() {}
+  constructor(private authService: AuthService) {}
 
   ngOnInit() {}
 
   register() {
-    console.log(this.model);
+    this.authService.register(this.model).subscribe(() => {
+      console.log('registration succesful');
+    }, error => {
+      console.log(error)
+    });
   }
 
   cancel() {
